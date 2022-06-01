5281e7709b779b1a475e86fd3af7b5b7b86ea045
RubenNijhuis <contact@rubennijhuis.com>
2022-06-01 16:15:47 +0200

    temp

---
+++ include/main.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.h                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rnijhuis <rnijhuis@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/06/01 15:51:26 by rnijhuis      #+#    #+#                 */
+/*   Updated: 2022/06/01 15:58:04 by rnijhuis      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef MAIN_H
+# define MAIN_H
+
+bool	parse_uint(const char *s, unsigned int *result,\
+	unsigned int min, unsigned int max);
+
+# endif
